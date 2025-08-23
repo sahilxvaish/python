d0cbb2d1adef17d74e8216a4f8995720509b1cb8
Sahil Vaish <sahilvxish@gmail.com>
2025-08-23 23:09:32 +0530

    py started

---
+++ datatyp.py
@@ -0,0 +1,7 @@
+name = "Sahil"
+age = 23
+price = 25.99
+
+print(type(name))
+print(type(age))
+print(type(price))
---
+++ variables.py
@@ -0,0 +1,12 @@
+name = "Sahil"
+age = 23
+price = 25.99
+
+print ("my name is" ,name)
+print ("My Age is" ,age)
+print (price)
+
+age2 = age
+
+print(age2)
+
