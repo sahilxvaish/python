48fa2b6a43e85fa741ba1ba010360915b6020cc9
Sahil Vaish <sahilvxish@gmail.com>
2025-08-24 22:44:30 +0530

    mfp

--- datatyp.py
+++ datatyp.py
@@ -1,7 +1,9 @@
 name = "Sahil"
 age = 23
 price = 25.99
+old = False 
 
 print(type(name))
 print(type(age))
 print(type(price))
+print (type(old))
---
+++ firstprog.py
@@ -0,0 +1,9 @@
+a = 1000
+b = 500
+sum = a + b
+sub = a - b 
+multiply = a * b
+divide = a / b
+
+
+print("The sum is", sum, "\nThe sub is", sub, "\nThe multiply is", multiply, "\nThe divide is", divide)
